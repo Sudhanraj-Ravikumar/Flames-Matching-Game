e283899487aa082ab91af37129427d7b38d2cb2c
sudhanraj ravikumar <sudhanraj.r@gmail.com>
2019-01-27 19:43:07 +0100

    Project Initialization

---
+++ FlamesGame/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlamesGame
+{
+    public partial class Form1 : Form
+    {
+        Func<string, string, string> Result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (label4.Text != "")
+            {
+                label4.Text = "";
+            }
+            string result;
+            string YourName = textBox1.Text.ToString();
+            string PartnerName = textBox2.Text.ToString();
+            MatchingLogic matchingLogic = new MatchingLogic();
+            if (YourName =="" || PartnerName == "")
+            {
+                MessageBox.Show("Please enter Name");
+            }
+            else
+	        {
+                result = matchingLogic.GetMatchingResult(YourName, PartnerName).ToString();
+                switch (result)
+                {
+                    case "f":
+                        label4.Text = "Friend";
+                        break;
+                    case "l":
+                        label4.Text = "Love";
+                        break;
+                    case "a":
+                        label4.Text = "Affection";
+                        break;
+                    case "m":
+                        label4.Text = "Marriage";
+                        break;
+                    case "e":
+                        label4.Text = "Enemy";
+                        break;
+                    case "s":
+                        label4.Text = "Sister/Brother";
+                        break;
+                } 
+            }
+            
+        }
+    }
+}
---
+++ FlamesGame/MatchingLogic.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlamesGame
+{
+    public class MatchingLogic
+    {
+        private string _x;
+        private string _y;
+        List<char> flames = new List<char>() { 'f', 'l', 'a', 'm', 'e', 's' };
+        string X { get => _x; set => _x = value; }
+        string Y { get => _y; set => _y = value; }
+        
+        public string GetString(string x,string y)
+        {
+            x = X;
+            y = Y;
+            return x + y;
+        }
+
+        
+        public string GetMatchingResult(string YourName,string PartnerName)
+        {
+            
+            string result ;
+            
+            int count;
+            char[] YourNameArray = YourName.ToCharArray();
+            char[] PartnerNameArrary = PartnerName.ToCharArray();
+
+            
+            count = GetmatchedLetterCount(YourNameArray, PartnerNameArrary);
+            return result = GetFlamesResultant(count);
+
+
+        }
+
+
+        private string GetFlamesResultant(int count)
+        {
+            List<char> Flames = flames;
+
+            if (count>5)
+            {
+                int mod = (int)Math.Floor((decimal)(count / 6));
+                return flames[mod].ToString(); 
+            }
+            return flames[count].ToString();
+        }
+
+        private int GetmatchedLetterCount(char[] MyName,char[] LastName)
+        {
+            List<char> myname =MyName.ToList();
+            List<char>lastname =  LastName.ToList();
+            int counter = 0;
+            for (int i = 0; i < myname.Count; i++)
+            {
+
+                if (lastname?.Count!=0)
+                {
+                    for (int j = 0; j < lastname.Count; j++)
+                    {
+                        if (myname[i] == lastname[j])
+                        {
+                            counter++;
+                            lastname.Remove(lastname[j]);
+                            lastname.TrimExcess();
+                            break;
+                        }
+
+                    }
+                }
+            }
+            return counter;
+        }
+    }
+}
